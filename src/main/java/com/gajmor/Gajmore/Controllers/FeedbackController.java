d73a25f96dab0a0e2f3cc50e5ebba80f0597f1b3
patilom-24 <omjagannathpatil24@gmail.com>
2025-12-28 23:07:05 +0530

    blogs all function ok and bcakend of feedback is added and working

---
+++ src/main/java/com/gajmor/Gajmore/Controllers/FeedbackController.java
@@ -0,0 +1,22 @@
+package com.gajmor.Gajmore.Controllers;
+
+import com.gajmor.Gajmore.Services.ServiceImpl.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.gajmor.Gajmore.Model.Feedback;
+
+@RestController
+@RequestMapping("/feedback")
+@CrossOrigin("*")
+public class FeedbackController {
+
+    @Autowired
+    private FeedbackService service;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addFeedback(@RequestBody Feedback feedback) {
+        return ResponseEntity.ok(service.saveFeedback(feedback));
+    }
+}
